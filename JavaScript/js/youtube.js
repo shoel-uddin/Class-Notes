c79ec5de102868dcf532c477876c2ed4ee4045b3
Shoel Uddin <shoel1989@gmail.com>
2020-11-05 09:40:38 -0500

    practice

--- JavaScript/js/youtube.js
+++ JavaScript/js/youtube.js
@@ -1,5 +1,21 @@
-callBeforeDeclared()
+let y= 10
+let x = 10
+let str5 = "";
 
-function callBeforeDeclared(){
-    console.log('Do something')
+for(let i = 1; i < y; i++ ){
+    for(let j = 1; j < x; j++){
+        if(i <= y / 2 && j >= (y / 2) - (i - 1) && j <= (y / 2) + (i - 1) ){
+            str5 = str5.concat("*");
+        }else if(i >= y / 2
+            && j > ((y / 2) -  i) * (-1)
+            && j < (y - ((y / 2) -  i) * (-1))){
+            str5 = str5.concat("*");
+        }
+        else {
+            str5 = str5.concat(" ");
+        }
+    }
+    str5 = str5.concat("\n");
 }
+
+console.log(str5)
